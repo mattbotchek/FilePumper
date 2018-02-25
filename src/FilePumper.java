b25cb78ba3020afddb431a3c19e7244c43bd0e63
MatthewBotchek <matthew@botchek.com>
2018-02-25 07:43:33 -0600

    First Commit

--- src/FilePumper.java
+++ src/FilePumper.java
@@ -1,3 +1,6 @@
+//Created by Matthew Botchek
+//LEAVE THIS IN ^^
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -5,45 +8,68 @@ import java.io.PrintWriter;
 import java.util.Random;
 
 public class FilePumper {
+		//This is a blank constructor because you don't need to pass any properties to it.
 		public FilePumper()
 		{
-
+		
 		}
+		/*
+		 * The createFile() method creates a single file wherever you want it to go
+		 * you supply the pathname and it does the rest of the work
+		 * The string you pass it upon calling the method is the pathname that the method 
+		 * will use to place a new file in.
+		 */
 		public void createFile(String f) throws IOException
 		{
 			String pathname = f;
 			File newFile = new File(pathname);
 			newFile.createNewFile();
 		}
+		/*
+		 * The infiniteFiles() method uses a boolean and an int to run.
+		 * The int is the amount of file created and the boolean controls if the files are created or not
+		 * (ie, it runs till the program terminates so be careful with this program)
+		 */
 		public void infiniteFiles(String pathname) throws IOException
 		{
 			int i = 1;
 			boolean run = true;
 			while (run = true)
 			{
-				String z = Integer.toString(i);
-				String f = pathname + z + ".txt";			
+				//This takes the i int and converts it to a string and then adds it to the string f
+				String f = pathname + i + ".txt";	
+				//the string f contains the pathname and the file name with a .txt at the end
 				File newFile = new File(f);
 				newFile.createNewFile();
 				i++;	
+				//This prints out the path to the file
 				System.out.println(f);
 			}
 		}
+		/*
+		 * The pumpFile() method takes in the pathname string that is passed to it and generates random
+		 * numbers within the file. Be careful with this as depending on whether your smart enough to make 
+		 * this recursive you could generate a large .txt file.
+		 */
 		public void pumpFile(String pathname) throws IOException
 		{
 			Random generator = new Random();
-			
+			//This PrintWriter using the name out takes the pathname that is passed and opens that file
 			PrintWriter out = new PrintWriter(new FileWriter(pathname));
 			boolean run = true;
 			while (run = true)
 			{
+				//This generates a random in up to the value of 9
 				int x = generator.nextInt(9);
+				//This prints it to the page
 				out.print(x);
+				//You could imagine that this is the save ability and calling it saves all the data onto the page.
 				out.flush();
 				System.out.println(x);
 			}
+			//This is also like a save ability and saves the data
 			out.close();
-			
+			//(I'm not sure which one actually saves the data into the .txt file.
 		}
 	}
 
--- src/FilePumperMain.java
+++ src/FilePumperMain.java
@@ -1,21 +1,28 @@
+//Created by Matthew Botchek
+//LEAVE THIS IN ^^
 import java.io.IOException;
 import java.util.Scanner;
 public class FilePumperMain {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		//This is the main class / this is where most methods are called and most interaction with the end user is done
 		System.out.println("1 to create a file, 2 to create a lot of files,");
 		System.out.println("3 to pump a file with an infinite amount of numbers");
 		System.out.println("Remember to add .txt to the end of the file pathname");	
 		FilePumper runner = new FilePumper();
+		//This scanner controls which options the user wants to use 
 		Scanner z = new Scanner(System.in);
 		int x = z.nextInt();
 		if (x == 1)
 		{
 			System.out.println("What is the path to where you want the file to be created?");
 			System.out.println("Include the filename name and .txt at the end of the path");
+			//I know that I could use the scanner created above but I found that it would sometimes cause problems
+			//So I just created a new one.
 			Scanner M = new Scanner(System.in);
 			String q = M.nextLine();
+			//This runs the createFile() method that is in the FilePumper.java Class
 			runner.createFile(q);
 		}
 		if (x == 2)
@@ -23,6 +30,7 @@ public class FilePumperMain {
 			System.out.println("What is the path to where the files should go?");
 			System.out.println("Do not include a name for the file or .txt");
 			System.out.println("Be sure to end the path with a /");
+			//Same issue as the other scanner above
 			Scanner M = new Scanner(System.in);
 			String q = M.nextLine();
 			runner.infiniteFiles(q);
@@ -31,6 +39,7 @@ public class FilePumperMain {
 		{
 			System.out.println("What is the path to the .txt file you want to pump?");
 			System.out.println("Be sure to include the filename and .txt in the path");
+			//Same issue with the other two scanners above.
 			Scanner M = new Scanner(System.in);
 			String a = M.nextLine();
 			runner.pumpFile(a);
